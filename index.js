4b5be853f2fd2418b7e822ed10c6fb4bb5b3d2d8
Petri Louhenheimo <petril@iki.fi@users.noreply.github.com>
2021-06-02 09:39:55 +0300

    Added some functionality

--- index.js
+++ index.js
@@ -7,9 +7,12 @@ const port = 3200;
 // GET, PUT, POST, DELETE
 
 // Main path / on webserver
-app.get("/", (req, res)=> {
+app.get("/products/:id", (req, res)=> {
     // res.send("Hello World");
-    res.json(Products)
+    // res.json(Products)
+    res.json(Products.find((product) => {
+        return +req.params.id === product.id
+    }))
 })
 
 app.listen(port, ()=> console.log("Listening on port " + port))
