72024b0a4a3da5302f80b1fb137fe892c7714280
Petri Louhenheimo <petril@iki.fi@users.noreply.github.com>
2021-06-02 09:29:49 +0300

    Added Products.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('hello world!')
+import Express from "express";
+import Products from "./products.js";
+
+const app = Express();
+const port = 3200;
+
+// GET, PUT, POST, DELETE
+
+// Main path / on webserver
+app.get("/", (req, res)=> {
+    // res.send("Hello World");
+    res.json(Products)
+})
+
+app.listen(port, ()=> console.log("Listening on port " + port))
